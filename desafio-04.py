44c70b958b9b4cd244b74f7074c3f9f6f291a9f5
Paula Silva <145219492+apaulasilva1@users.noreply.github.com>
2023-09-21 21:41:58 -0300

    Add files via upload

---
+++ desafio-04.py
@@ -0,0 +1,8 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('Só tem espaços? ', a.isspace())
+print('é um número? ', a.isnumeric())
+print('é alfabético? ', a.isalpha())
+print('é alfanumérico? ', a.isalnum())
+print('esta em maiúscula? ', a.isupper())
+print('esta em minúsculo? ', a.islower())
---
+++ desafio-07.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+m = (n1 + n2) / 2
+print('A média entre {} e {} é igual a {}'.format(n1, n2, m))
